a8269053c66eeef6c97f75524ecbfcb37fb5eb57
loohopen <loohopen@163.com>
2022-10-07 05:19:54 +0800

    ok

--- src/shanyangtianyuan/js/index.js
+++ src/shanyangtianyuan/js/index.js
@@ -2,15 +2,16 @@ window.onload = function () {
   var submenu = document.getElementsByClassName("submenu");
   var box = document.getElementsByClassName("box");
   var boxPage = document.getElementsByClassName("box-page");
-  var coldCard = boxPage[0]. getElementsByClassName("card");
-  var hotCard=boxPage[1]. getElementsByClassName("card");
+  var coldCard = boxPage[0].getElementsByClassName("card");
+  var hotCard=boxPage[1].getElementsByClassName("card");
+  var riceCard=box[1].getElementsByClassName("card");
+  var afteaCard=box[2].getElementsByClassName("card");
   var cookingStep = document.getElementsByClassName("cooking-step")[0];
   var content = document.getElementsByClassName("content")[0];
   var holiday=document.getElementsByClassName("holiday")[0];
-  var cookingimg=document.getElementsByClassName("cooking-img")[0];
   var cookingText=document.getElementsByClassName("cooking-text")[0];
-  var stepList=document.getElementsByClassName("step-list")[0];
-  var weekDays = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'];
+  var holiday=document.getElementsByClassName("holiday")[0];
+  var weekDays = ['/shanyangtianyuan/images/day1.png', '/shanyangtianyuan/images/day2.png', '/shanyangtianyuan/images/day3.png', '/shanyangtianyuan/images/day4.png', '/shanyangtianyuan/images/day5.png', '/shanyangtianyuan/images/day6.png', '/shanyangtianyuan/images/day7.png'];
   //查找冷菜热菜
   var coldFood = [];
   var hotFood = [];
@@ -37,7 +38,7 @@ window.onload = function () {
   for (var a = 0; a < hotFood.length; a++) {
     myhotMenu += '<div class="card">' +
       '<div class="card-text">' +
-      '<div class="card-phtoto" style="background-image: url(' + hotFood[a]['img'] + ')"></div>' +
+      '<div class="card-phtoto" style="background-image: url(' + hotFood[a]['img1'] + ')"></div>' +
       '<div class="card-bottom">' + hotFood[a]['name'] + '</div>' +
       '</div>' +
       '<div class="card-bg">' +
@@ -55,7 +56,7 @@ window.onload = function () {
       '<div class="card-text">' +
       '<div class="card-header">' +
       '<div class="header-left">' +
-      '<div class="week">' + weekDays[a] + '</div>' +
+      '<img class="week" src="'+ weekDays[a] +'" />'+
       '<div class="date"></div>' +
       '</div>' +
       ' <div class="header-text">' + riceMenu[a]['name'] + '</div>' +
@@ -87,7 +88,6 @@ window.onload = function () {
   }
   box[2].innerHTML = myafternoonTea;
 
-
   //默认
   box[0].className = "box active";
   submenu[0].className = "submenu activebg";
@@ -99,6 +99,7 @@ window.onload = function () {
       arr[arrIndex].className = "box active";
       arr1[arrIndex].className = "submenu activebg";
     }
+    holiday.className = 'holiday holiday-' + (arrIndex+1);
   }
   //点击菜单切换菜品
   for (var a = 0; a < submenu.length; a++) {
@@ -112,18 +113,39 @@ window.onload = function () {
 
   //点击图片显示制作步骤
   function click (arr,arr1) {
+    console.log(arr1)
     for (var a = 0; a < arr.length; a++) {
       arr[a].index = a;
       arr[a].onclick = function () {
         console.log(this.index, 'index');
         cookingStep.className = 'cooking-step show';
-        cookingimg.src=arr1[this.index]['img'];
+        var stepList=document.getElementsByClassName("step-list")[0];
+        var cookingimg=document.getElementsByClassName("cooking-img")[0];
+        var addressHeader=document.getElementsByClassName("address-header")[0];
+        var addressFoot=document.getElementsByClassName("address-foot")[0];
+        var cookingStepCen=document.getElementsByClassName("cooking-step-cen")[0];
+        cookingimg.src=arr1[this.index]['img2'];
         cookingText.innerHTML=arr1[this.index]['desc'];
         stepList.innerHTML=arr1[this.index]['cookie'];
+        addressHeader.innerHTML=arr1[this.index]['advice'];
+        addressHeader.href = 'https://uri.amap.com/marker?position='+arr1[this.index]['position']+'&name='+arr1[this.index]['advice']+'&coordinate=gaode&callnative=1';
+        addressFoot.innerHTML=arr1[this.index]['address'];
+       cookingStepCen.innerHTML=arr1[this.index]['name']+'制作方法';
+
+        //关闭做菜步骤
+        var close=document.getElementsByClassName("close")[0];
+        close.onclick=function(){
+          cookingStep.className='cooking-step';
+        }
+        
       }
     }
   }
   click(coldCard,coldFood);
   click(hotCard,hotFood);
+  click(riceCard,riceMenu);
+  click(afteaCard,afternoonTea);
+ 
+  
 
 }
